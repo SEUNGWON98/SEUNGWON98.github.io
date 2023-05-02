1e1489aa2b8011cdb4e2894571c1d491fc7ef40d
SEUNGWON98 <98tjtmddnjs@Naver.com>
2023-05-02 17:01:02 +0900

    first step

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-function Clock() {
-  const today = new Date();
-  const month = today.toLocaleString("en-US", { month: "short" });
-  const date = today.getDate();
-  const hour = today.getHours();
-  const minute = today.getMinutes();
-  const minutes = minute < 10 ? "0" + minute : minute.toString();
-  const clock = document.querySelector(".clock");
+const loginForm = document.querySelector(".login_form");
+const loginInput = document.querySelector(".login_form_id");
+const greeting = document.querySelector("#greeting");
 
-  clock.innerHTML = `${month} ${date} <br/> <strong class="hours"> ${hour}: ${minutes} <strong/> `;
+const empty = document.querySelector(".warning");
+
+const HIDDEN_CLASSNAME = "hidden";
+const ID_KEY = "loginId";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const loginId = loginInput.value;
+  // loginForm.classList.add(HIDDEN_CLASSNAME);
+  // console.log(loginId);
+  localStorage.setItem(ID_KEY, loginId);
+
+  greeting.innerHTML = `Hello ${loginId}`;
+  // greeting.classList.remove(HIDDEN_CLASSNAME);
+
+  if (savedUsername == "") {
+    // loginForm.classList.remove(HIDDEN_CLASSNAME);
+
+    empty.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+  } else {
+    // greeting.innerHTML = `Hello ${savedUsername}`;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // function clock() {
+    //   const today = new Date();
+    //   const month = today.toLocaleString("en-US", { month: "short" });
+    //   const date = today.getDate();
+    //   const hour = today.getHours();
+    //   const minute = today.getMinutes();
+    //   const minutes = minute < 10 ? "0" + minute : minute.toString();
+    //   const clock = document.querySelector(".clock");
+    // }
+    // clock.innerHTML = `${month} ${date} <br/> <strong class="hours"> ${hour}: ${minutes} <strong/> `;
+    // clock.classList.remove(HIDDEN_CLASSNAME);
+    // setInterval(Clock, 1000);
+  }
 }
 
-setInterval(Clock, 1000);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(ID_KEY);
+console.log(savedUsername);
