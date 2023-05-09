7e00bc3be99d59319e17fe5a8576936bde8e4d3c
SEUNGWON98 <98tjtmddnjs@Naver.com>
2023-05-09 16:22:11 +0900

    main

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 const loginForm = document.querySelector(".login_form");
 const loginInput = document.querySelector(".login_form_id");
 const greeting = document.querySelector("#greeting");
+const main = document.querySelector(".main");
 
 const empty = document.querySelector(".warning");
 
 const HIDDEN_CLASSNAME = "hidden";
 const ID_KEY = "loginId";
+// const loginId = loginInput.value;
+
+const savedUsername = localStorage.getItem(ID_KEY);
+
+const clock = document.querySelector(".clock");
+
+main.classList.add(HIDDEN_CLASSNAME);
+
+function Clock() {
+  const today = new Date();
+  const month = today.toLocaleString("en-US", { month: "short" });
+  const date = today.getDate();
+  const hour = today.getHours();
+  const minute = today.getMinutes();
+  const minutes = minute < 10 ? "0" + minute : minute.toString();
+
+  clock.innerHTML = `${month} ${date} <br/> <strong class="hours"> ${hour}: ${minutes} <strong/> `;
+  // clock.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
-  event.preventDefault();
   const loginId = loginInput.value;
-  // loginForm.classList.add(HIDDEN_CLASSNAME);
-  // console.log(loginId);
   localStorage.setItem(ID_KEY, loginId);
-
-  greeting.innerHTML = `Hello ${loginId}`;
-  // greeting.classList.remove(HIDDEN_CLASSNAME);
-
-  if (savedUsername == "") {
-    // loginForm.classList.remove(HIDDEN_CLASSNAME);
+  if (loginInput.value == "") {
+    event.preventDefault();
 
     empty.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
   } else {
-    // greeting.innerHTML = `Hello ${savedUsername}`;
+    event.preventDefault();
+
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
+    greeting.innerHTML = `환영합니다! ${loginId} 님`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    // function clock() {
-    //   const today = new Date();
-    //   const month = today.toLocaleString("en-US", { month: "short" });
-    //   const date = today.getDate();
-    //   const hour = today.getHours();
-    //   const minute = today.getMinutes();
-    //   const minutes = minute < 10 ? "0" + minute : minute.toString();
-    //   const clock = document.querySelector(".clock");
-    // }
-    // clock.innerHTML = `${month} ${date} <br/> <strong class="hours"> ${hour}: ${minutes} <strong/> `;
-    // clock.classList.remove(HIDDEN_CLASSNAME);
-    // setInterval(Clock, 1000);
+    Clock();
+    clock.classList.remove(HIDDEN_CLASSNAME);
+    main.classList.remove(HIDDEN_CLASSNAME);
   }
 }
 
+console.log(savedUsername);
+
+// loginForm.addEventListener("submit", Clock);
 loginForm.addEventListener("submit", onLoginSubmit);
 
-const savedUsername = localStorage.getItem(ID_KEY);
-console.log(savedUsername);
+setInterval(Clock, 1000);
