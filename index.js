e3e717ded5b9918a62e855bb41a702886ade875c
SEUNGWON98 <98tjtmddnjs@Naver.com>
2023-05-10 18:29:13 +0900

    랜덤 배경화면

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector(".login_form");
 const loginInput = document.querySelector(".login_form_id");
 const greeting = document.querySelector("#greeting");
 const main = document.querySelector(".main");
+const body = document.querySelector("body");
 
 const empty = document.querySelector(".warning");
 
@@ -19,6 +20,15 @@ const todolist = document.querySelector(".todolist");
 const todolist_input = document.querySelector(".todolist_input");
 const lists = document.querySelector("ul");
 let todos = [];
+
+const images = ["beach.jpg", "car.jpg", "cat.jpg", "nature.jpg", "thunder.jpg"];
+
+function setBackground() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const selectedImage = images[randomIndex];
+  const backgroundImage = `url("images/${selectedImage}")`;
+  body.style.backgroundImage = backgroundImage;
+}
 
 function addTodo() {
   event.preventDefault();
@@ -73,6 +83,7 @@ function onLoginSubmit(event) {
     greeting.innerHTML = `환영합니다! ${loginId} 님`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     Clock();
+    setBackground();
     clock.classList.remove(HIDDEN_CLASSNAME);
     main.classList.remove(HIDDEN_CLASSNAME);
   }
@@ -86,3 +97,25 @@ loginForm.addEventListener("submit", onLoginSubmit);
 loadTodos();
 
 setInterval(Clock, 1000);
+
+//======================================
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const API_key = "3cfe0eba7d5658c6f07afef5eb85a86a";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&lang=kor&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerHTML = data.name;
+      weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
